69dc2e27ab95c1eeea97a35e04a9d937351bdc2c
Pedro Sol <pedrosolbm@gmail.com>
2019-12-19 01:40:52 -0300

    initial commit

---
+++ src/test/java/com/pedrosolbm/crud/api/CrudStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosolbm.crud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
